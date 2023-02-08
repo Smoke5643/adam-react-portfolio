f9de9185364fd56bf2ed81187624dd4e7c950d87
Smoke5643 <smoke5643@gmail.com>
2023-02-08 17:03:14 -0500

    added email validator and logic to handle inputs in contact page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -1,26 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
+import emailValidator from '../../utils/emailValidator'
 
 export default function Contact() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [error, setError] = useState('');
+
+    const handleChange = (e) => {
+        const { target } = e;
+        const inputName = target.name;
+        const inputValue = target.value
+
+        if (inputName === 'name') {
+            setName(inputValue);
+        } else if (inputName === 'email') {
+            setEmail(inputValue);
+        } else {
+            setMessage(inputValue);
+        }
+    }
+
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+
+        if (!name && !email && !message) {
+            setError('All fields required for submission!')
+        }
+        if (!name) {
+            setError('Enter your name to continue!')
+        }
+        if (!email && !emailValidator(email)) {
+            setError('Enter a valid email to continue!')
+        } 
+        if (!message) {
+            setError('Enter a message to continue!')
+        }
+
+        setName('');
+        setEmail('');
+        setMessage('');
+    }
+
     return (
         <form>
             <fieldset>
                 <div className='row justify-content-center'>
                     <div className='col-6'>
-                        <div class="form-group">
-                            <label class="col-form-label mt-4" for="inputDefault">Name:</label>
-                            <input type="text" class="form-control" placeholder="Enter your name" id="inputDefault" />
+                        <div className="form-group">
+                            <label name='name' class="col-form-label mt-4" for="inputDefault">Name:</label>
+                            <input onChange={handleChange} type="text" className="form-control" placeholder="Enter your name" id="inputDefault" />
                         </div>
                         <div className="form-group">
                             <label for="inputEmail" className="form-label mt-4">Email address:</label>
-                            <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter your email address" />
+                            <input name='email'onChange={handleChange} type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter your email address" />
                         </div>
                         <div class="form-group">
                             <label for="exampleTextarea" class="form-label mt-4">Message:</label>
-                            <textarea class="form-control" id="exampleTextarea" rows="3" placeholder='Enter your message'></textarea>
+                            <textarea name='message'onChange={handleChange} className="form-control" id="exampleTextarea" rows="3" placeholder='Enter your message'></textarea>
                         </div>
-                        <button type="button" class="btn btn-primary mt-4">Submit</button>
+                        <button onClick={handleFormSubmit} type="button" className="btn btn-primary mt-4">Submit</button>
                     </div>
                 </div>
+                {error && (
+                    <div className="row text-center">
+                        <p>{error}</p>
+                    </div>
+                )}
             </fieldset>
         </form>
     );
--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,54 +1,60 @@
 import React from 'react';
 import noteTaker from '../../assets/note_taker.png'
 import weatherDashboard from '../../assets/weather_dashboard.png'
+import sofaKing from '../../assets/sofa_king.png'
+import socialMedia from '../../assets/social_media.gif'
+import ecommerce from '../../assets/e-commerce.gif'
+import employeeTracker from '../../assets/employee-tracker.gif'
 
 export default function Portfolio() {
   return (
     <div className='container'>
-      <div className='row  my-3 justify-content-center'>
+      <div className='my-3'>
         <h2>My Portfolio</h2>
         <br />
         <br />
-        <div className='col-4 card border border-secondary'>
-          <a className='m-2' href="https://pacific-wildwood-35009.herokuapp.com/" target='_blank' rel="noreferrer">Deployed Application</a>
+      </div>
+      <div className='row my-3 justify-content-center'>
+        <div className='col-4 my-2 me-2 card border border-secondary'>
+          <a className='m-2' href="https://quiet-river-37193.herokuapp.com/" target='_blank' rel="noreferrer">Sofa King</a>
+          <img src={sofaKing} alt="Note Taker app" />
+          <div className='card-body'>
+            <a href="https://github.com/kassrojas/sofa-king-cool" target='_blank' rel="noreferrer">GitHub Repository</a>
+          </div>
+        </div>
+        <div className='col-4 my-2 card border border-secondary'>
+          <a className='m-2' href="https://pacific-wildwood-35009.herokuapp.com/" target='_blank' rel="noreferrer">Note Taker</a>
           <img src={noteTaker} alt="Note Taker app" />
           <div className='card-body'>
             <a href="https://github.com/Smoke5643/note-taker" target='_blank' rel="noreferrer">GitHub Repository</a>
           </div>
         </div>
-        <div className='col-4 card border border-secondary'>
-          <a className='m-2' href="https://smoke5643.github.io/weather-dashboard/" target='_blank' rel="noreferrer">Deployed Application</a>
+        <div className='col-4 my-2 card border border-secondary'>
+          <a className='m-2' href="https://smoke5643.github.io/weather-dashboard/" target='_blank' rel="noreferrer">Weather Dashboard</a>
           <img src={weatherDashboard} alt="Note Taker app" />
           <div className='card-body'>
             <a href="https://github.com/Smoke5643/weather-dashboard" target='_blank' rel="noreferrer">GitHub Repository</a>
           </div>
         </div>
-        <div className='col-4 card border border-secondary'>
-          <a className='m-2' href="https://pacific-wildwood-35009.herokuapp.com/" target='_blank' rel="noreferrer">Deployed Application</a>
-          <img src={noteTaker} alt="Note Taker app" />
+        <div className='col-4 my-2 card border border-secondary'>
+          <a className='m-2' href="https://drive.google.com/file/d/1TejKQjwF-qJIXWneMWBSH9KkTvxl48Gd/view" target='_blank' rel="noreferrer">Social Media API Video Walkthrough</a>
+          <img src={socialMedia} alt="Note Taker app" />
           <div className='card-body'>
-            <a href="https://github.com/Smoke5643/note-taker" target='_blank' rel="noreferrer">GitHub Repository</a>
+            <a href="https://github.com/Smoke5643/social-media-api" target='_blank' rel="noreferrer">GitHub Repository</a>
           </div>
         </div>
-        <div className='col-4 card border border-secondary'>
-          <a className='m-2' href="https://pacific-wildwood-35009.herokuapp.com/" target='_blank' rel="noreferrer">Deployed Application</a>
-          <img src={noteTaker} alt="Note Taker app" />
+        <div className='col-4 my-2 card border border-secondary'>
+          <a className='m-2' href="https://drive.google.com/file/d/1a0494cENN64bKb94ctxINzm8sVgSX4Tf/view" target='_blank' rel="noreferrer">E-Commerce Backend Video Walkthrough</a>
+          <img src={ecommerce} alt="Note Taker app" />
           <div className='card-body'>
-            <a href="https://github.com/Smoke5643/note-taker" target='_blank' rel="noreferrer">GitHub Repository</a>
+            <a href="https://github.com/Smoke5643/e-commerce-backend" target='_blank' rel="noreferrer">GitHub Repository</a>
           </div>
         </div>
-        <div className='col-4 card border border-secondary'>
-          <a className='m-2' href="https://pacific-wildwood-35009.herokuapp.com/" target='_blank' rel="noreferrer">Deployed Application</a>
-          <img src={noteTaker} alt="Note Taker app" />
+        <div className='col-4 my-2 card border border-secondary'>
+          <a className='m-2' href="https://drive.google.com/file/d/12LTUjh2h-JAjHUPF92JPyocMNon-jYIv/view" target='_blank' rel="noreferrer">Employee Tracker Video Walkthrough</a>
+          <img src={employeeTracker} alt="Note Taker app" />
           <div className='card-body'>
-            <a href="https://github.com/Smoke5643/note-taker" target='_blank' rel="noreferrer">GitHub Repository</a>
-          </div>
-        </div>
-        <div className='col-4 card border border-secondary'>
-          <a className='m-2' href="https://pacific-wildwood-35009.herokuapp.com/" target='_blank' rel="noreferrer">Deployed Application</a>
-          <img src={noteTaker} alt="Note Taker app" />
-          <div className='card-body'>
-            <a href="https://github.com/Smoke5643/note-taker" target='_blank' rel="noreferrer">GitHub Repository</a>
+            <a href="https://github.com/Smoke5643/employee-tracker" target='_blank' rel="noreferrer">GitHub Repository</a>
           </div>
         </div>
       </div>
